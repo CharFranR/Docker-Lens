c407c6c3c192fb15f1870b5ea0826a125da546b8
Oscar Francisco Reyes Guevara <oscar.reyes@est.ulsa.edu.ni>
2026-05-21 14:51:18 -0600

    cli primera implementacion

--- python/docker_lens/cli.py
+++ python/docker_lens/cli.py
@@ -1,10 +1,64 @@
 import docker_lens
+import click
+import os
 
-credenciales = docker_lens.find_db_py("/home/frandev/Documentos/Proyecto-Asignatura-Web")
+@click.group()
+def cli():
+    """Docker-Lens: La puritita calidad"""
+    pass
 
-print (credenciales)
+#"/home/frandev/Documentos/Proyecto-Asignatura-Web"
+
+@cli.command()
+@click.argument("path", required=True)
+def info(path):
+    # docker-lens info [path]
+
+    if path == ".":
+        path = os.getcwd()
+
+    try:
+        orchestator_path = docker_lens.find_orchestrator_py(path)
+    except RuntimeError:
+        click.echo("No se encontró un archivo docker-compose.yml en este proyecto", err=True)
+        return
+    
+    try:
+        credenciales = docker_lens.find_db_py(orchestator_path)
+        click.echo(credenciales)
+    except RuntimeError:
+        click.echo("Base de datos inaccesible", err=True)
+  
 
 
-tablas = docker_lens.list_tables_py(credenciales)
+@cli.command()
+@click.argument("path", required=True)
+def tables(path):
+    # docker-lens tables [path]
 
-print(tablas)
+    if path == ".":
+        path = os.getcwd()
+
+    try:
+        orchestator_path = docker_lens.find_orchestrator_py(path)
+    
+    except RuntimeError:
+        click.echo("No se encontró un archivo docker-compose.yml en este proyecto", err=True)
+        return
+    
+    try:
+        credenciales = docker_lens.find_db_py(orchestator_path)
+        tablas = docker_lens.list_tables_py(credenciales)
+        click.echo(tablas)
+
+    except RuntimeError:
+        click.echo("Base de datos inaccesible", err=True)
+    
+
+@cli.command()
+def query():
+    # docker-lens query "" [path]
+    print("Aqui te va la response")
+
+if __name__ == '__main__':
+    cli()
