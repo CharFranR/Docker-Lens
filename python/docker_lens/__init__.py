f6fa94fe421da6b54332404970fd16743dc43b8f
CharFranR <oscar.reyes@est.ulsa.edu.ni>
2026-06-11 18:10:39 -0600

    fix: read version from package metadata instead of hardcoding

--- python/docker_lens/__init__.py
+++ python/docker_lens/__init__.py
@@ -1,3 +1,7 @@
 from .docker_lens import *
 
-__version__ = "0.1.0"
+try:
+    from importlib.metadata import version as _get_version
+    __version__ = _get_version("dlens-py")
+except Exception:
+    __version__ = "0.0.0"
